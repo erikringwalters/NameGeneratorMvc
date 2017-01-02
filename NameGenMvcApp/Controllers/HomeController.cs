6feba92e518087785d386e6dd0a354fb5670586e
Erik <ering-walters@csumb.edu>
2017-01-01 21:21:55 -0800

    initial

---
+++ NameGenMvcApp/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NameGenMvcApp.Controllers;
+using NameGenMvcApp.Models;
+
+namespace NameGenMvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult CharacterInfoForm()
+        {
+            return View(new UserResponse());
+        }
+
+        [HttpPost]
+        public ViewResult CharacterInfoForm(UserResponse userResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                userResponse.GeneratedName = "Name is generated for " + userResponse.Race + " " + userResponse.Sex;
+                return View(userResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    };
+}
---
+++ NameGenMvcApp/Models/UserResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NameGenMvcApp.Controllers;
+using System.ComponentModel.DataAnnotations;
+
+namespace NameGenMvcApp.Models
+{
+    public class UserResponse
+    {
+        public String Race { get; set; }
+        public String GeneratedName { get; set; }
+        public String Sex { get; set; }
+    }
+
+   
+}
